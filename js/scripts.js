32d2c5d2f61c4cb844750863dd5c0ee8ef9915e9
John Kelly <jlfkelly@gmail.com>
2019-06-05 13:15:09 +0200

    Created repository
    Repository of Pokemon consisting of an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-alert('Hello world');
+var repository = [
+  ['Bulbasaur',
+  0.7,
+  ['Grass', 'Poison']],
+
+  ['Pidgey',
+  0.3,
+  ['Flying', 'Normal']],
+
+  ['Zubat',
+  0.8,
+  ['Poison', 'Flying']],
+
+  ['Jigglypuff',
+  0.5,
+  ['Fairy', 'Normal']],
+  ];
