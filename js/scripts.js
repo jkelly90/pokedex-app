c210b288a59d79a5934b7e42839ffaa3742d28ee
John Kelly <jlfkelly@gmail.com>
2019-06-06 09:24:58 +0200

    Added if conditional
    conditional check which pokemon is biggest

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 var repository = [
   ['Bulbasaur',
   0.7,
-  ['Grass', 'Poison']],
+  ['grass', ' poison']],
 
   ['Pidgey',
   0.3,
-  ['Flying', 'Normal']],
+  ['flying', ' normal']],
 
   ['Zubat',
   0.8,
-  ['Poison', 'Flying']],
+  ['poison', ' flying']],
 
   ['Jigglypuff',
   0.5,
-  ['Fairy', 'Normal']],
+  ['fairy', ' normal']],
   ];
 
   for (var i = 0; i < repository.length; i++){
-    document.write('<p>' + repository[i][0]  +  ' (height: ' + repository[i][1] + ')' + '<p>');
+    document.write('<p>' + repository[i][0] + '<br>' + repository[i][2] + '<br>' +  'height: ' + repository[i][1] + '<p>');
+   if (repository[i][1] >= 0.8) {
+        document.write('Wow, that\'s big!');
+    }
   }
